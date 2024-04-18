15c41717f5c3b4101d32cb9d18f196191bde4e0a
fyodor <fyodor.yakimchouk@icloud.com>
2024-04-18 18:23:39 +1200

    add DynamoDB definition

--- backend/hello-world/routes.ts
+++ backend/hello-world/routes.ts
@@ -1,14 +1,75 @@
 import { Request, Response, Router } from 'express';
+import AWS from 'aws-sdk';
 
 const router = Router();
 
+const userDataTableName = process.env.UserDataTableName;
+
+const local = process.env.LOCAL === 'true';
+let endpoint: string | undefined;
+
+if (local) {
+    endpoint = 'http://host.docker.internal:4566';
+}
+
 router.get('/hello', async (req: Request, res: Response) => {
     try {
-        res.status(200).json({ message: 'double ok' });
+        res.status(200).json({ message: 'initialised' });
     } catch (error) {
         console.error('An error ocurred:', error);
         res.status(500).json(error);
     }
 });
+
+router.post('/save-data', async (req: Request, res: Response) => {
+    const dynamodb = new AWS.DynamoDB({
+        endpoint
+    });
+
+    const auth = req.auth!;
+
+    const params = {
+        TableName: userDataTableName,
+        Item: {
+            id : { S: auth.payload.sub},
+            data: { S:req.body.data}
+        }
+    };
+
+    await dynamodb.putItem(params).promise();
+
+    res.status(200).json({ message: 'saved' });
+});
+
+router.get('/load-data', async (req: Request, res: Response) => {
+    const docClient = new AWS.DynamoDB.DocumentClient({
+        endpoint
+    });
+
+    const auth = req.auth!;
+
+    const params = {
+        TableName: userDataTableName,
+        KeyConditionExpression: "#id = :id",
+        ExpressionAttributeNames:{
+            "#id": "id"
+        },
+        ExpressionAttributeValues: {
+            ":id": auth.payload.sub
+        }
+    }
+
+    console.log('getting data...');
+    const result = await docClient.query(params).promise();
+    let loadedData = '';
+    if (result.Items) {
+        if (result.Items[0]) {
+            loadedData = result.Items[0].data;
+        }
+    }
+
+    res.status(200).json({ data: loadedData });
+});
+
 
 export default router;
--- frontend/src/routes/calculator.svelte
+++ frontend/src/routes/calculator.svelte
@@ -4,19 +4,50 @@
     import {useAuth0} from "../services/auth0";
 
     let { getAccessToken } = useAuth0;
-    let greeting: {message: string};
+    let message: string;
+    let loading = true;
+    let dataValue = '';
 
     onMount(async () => {
         const token = await getAccessToken({ authorizationParams: { audience: import.meta.env.VITE_API_URL }});
-        console.log(token);
         const response = await axios.get(`${import.meta.env.VITE_API_URL}/hello`, {
             headers: {
                 Authorization: 'Bearer ' + token
             }
         });
+        message = response.data.message;
+        loading = false;
+    });
 
-        greeting = response.data;
-    })
+    async function loadData() {
+        loading = true;
+        const token = await getAccessToken({ authorizationParams: { audience: import.meta.env.VITE_API_URL }});
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/load-data`, {
+            headers: {
+                Authorization: 'Bearer ' + token
+            }
+        });
+        message = 'loaded';
+        dataValue = response.data.data;
+        loading = false;
+    }
+
+    async function saveData() {
+        loading = true;
+        const token = await getAccessToken({ authorizationParams: { audience: import.meta.env.VITE_API_URL }});
+        await axios.post(`${import.meta.env.VITE_API_URL}/save-data`, { data: dataValue }, {
+            headers: {
+                Authorization: 'Bearer ' + token
+            }
+        });
+        message = 'saved';
+        loading = false;
+    }
 </script>
 
-<p>{greeting ? greeting.message : 'Loading calculator'}</p>
+{#if !loading}
+    <p>Message: {message}</p>
+    <p><textarea bind:value={dataValue}></textarea></p><p><button on:click={() => loadData()}>Load</button> <button on:click={() => saveData()}>Save</button></p>
+{:else}
+    <p>Loading...</p>
+{/if}
